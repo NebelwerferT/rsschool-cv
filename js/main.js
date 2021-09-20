e2565e83495303ff7cb1825b557ca2683b2357c1
NebelwerferT <NebelwerferT9@gmail.com>
2021-09-21 01:41:42 +0400

    feat: add slow animation for burger-menu

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ const navMenu = document.querySelector(".header-nav__list");
 
 viewBurgerMenuButton.addEventListener("click", () => {
     navMenu.style.display = "flex";
+    navMenu.classList.toggle('anim');
     viewBurgerMenuButton.style.display = "none";
     closeBurgerMenuButton.style.display = "block";
 });
 
 closeBurgerMenuButton.addEventListener("click", () => {
     navMenu.style.display = "";
+    navMenu.classList.toggle('anim');
     viewBurgerMenuButton.style.display = "block";
     closeBurgerMenuButton.style.display = "none";
 })
